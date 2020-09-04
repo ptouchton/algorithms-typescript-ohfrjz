659257c8f364f3486a0561eb2df20c50fd513483
ptouchton <p_touchton@yahoo.com>
2020-09-04 16:45:13 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,38 @@
-// Import stylesheets
-import './style.css';
+// Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
+// a simple multiplication fn
+export function times10(n: number): number {
+
+  return n*10;
+
+};
+
+console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
+console.log('times10 returns:', times10(9));
+
+// Task 2: Use an object to cache the results of your times10 function. 
+// protip 1: Create a function that checks if the value for n has been calculated before.
+// protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
+
+let timesTen = times10(9);
+const cache: Object = {};
+cache[timesTen] = true;
+
+export function memoTimes10(n: number): void {
+
+  if (!cache[n]){
+     let res = times10(n)
+     cache[res] = true;
+  }
+
+}
+
+console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
+console.log('Task 2 calculated value:', memoTimes10(9));	// calculated
+console.log('Task 2 cached value:', memoTimes10(9));	// cached
+
+
+
+
 
  export function uniqSort(arr: Array<number>): Array<number> {
 
