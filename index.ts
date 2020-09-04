e28e95b6e3c250e9aa8ee0114a9110919196cb84
ptouchton <p_touchton@yahoo.com>
2020-09-04 16:54:10 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -17,42 +17,15 @@ let timesTen = times10(9);
 const cache: Object = {};
 cache[timesTen] = true;
 
-export function memoTimes10(n: number): void {
-
-  if (!cache[n]){
-     let res = times10(n)
-     cache[res] = true;
-  }
+export function memoTimes10(n: number): number {
+  let res: number = times10(n);
+  if (!cache[res]) cache[res] = true;
+  
+  console.log(cache)
+  return cache[res];
 
 }
 
 console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
 console.log('Task 2 calculated value:', memoTimes10(9));	// calculated
 console.log('Task 2 cached value:', memoTimes10(9));	// cached
-
-
-
-
-
- export function uniqSort(arr: Array<number>): Array<number> {
-
-  const breadCrumbs: Object = {};
-  let result: Array<number> = [arr[0]];
-
-  for (let i of arr) {
-
-    if (!breadCrumbs[arr[i]]) {
-
-      result.push(arr[i]);
-      breadCrumbs[arr[i]] = true;
-    }
-  }
-  // console.log(breadCrumbs);
-  // console.log(result);
-  
-  return result.sort((a,b) => a - b);
-
-}
-
-let ret = uniqSort([4,2,2,3,2,2,2]);
-console.log(ret);
