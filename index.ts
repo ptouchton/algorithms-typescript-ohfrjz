0751d4d5fd35f82fae66eba7ac21ab93bef1bd44
ptouchton <p_touchton@yahoo.com>
2020-09-06 10:07:59 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -1,25 +1,17 @@
-// Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
-// a simple multiplication fn
-const times10 = (n: number): number => n*10
+// Task 1: make it a closure function
+const memoTimes10 = (() => {
+  let cache: Object = {};
 
-console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
-console.log('times10 returns:', times10(9));
-
-// Task 2: Use an object to cache the results of your times10 function. 
-// protip 1: Create a function that checks if the value for n has been calculated before.
-// protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
-
-const cache: Object = {};
-
-const memoTimes10 = (n: number): number => {
+    return (n: number) => {
+ 
+    if (!cache[n]) cache[n] = n * 10;
   
-  //let res: number = times10(n);
-  if (!cache[n]) cache[n] = times10(n);
-  
-  console.log(cache)
-  return cache[n];
+    console.log(cache)
+    return cache[n];
 
-}
+  }
+
+})();
 
 console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
 console.log('Task 2 calculated value:', memoTimes10(9));	// calculated
