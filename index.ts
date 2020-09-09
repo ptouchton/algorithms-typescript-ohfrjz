5ef79bd2545faa4eccedd186b42a52745a5943f1
ptouchton <p_touchton@yahoo.com>
2020-09-08 21:01:24 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
-const linearSearch = (list: Array<number>,num: number) => {
+const bubbleSort = (list: Array<number>): Array<number> => {
+  let res: Array<number>;
   
-   for (let i of list) {
-     console.log(i);
-     if (i === num) {
-       console.log(`found ${num}`);
-       return;
-     }
-   } 
+  for (let i = 0; i < list.length; i++) {
+
+    if (list[i] > list[i+1]) {
+      let rem = list.splice(i+1, 1);
+      console.log(rem);
+      list.splice(i,0,rem[0]);
+    } 
+  }
+
+  console.log(list);
+  return res;
 
 }
 
 
-linearSearch([2,6,7,90,103], 90);
+let res: Array<number> = bubbleSort([7,2,9,4,3,10,5]);
+console.log(res)
